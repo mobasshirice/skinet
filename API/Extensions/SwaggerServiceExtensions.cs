249b6d3a6a420e170465d5393e536e5c582cdd2b
unknown <mobasshirice@gmail.com>
2023-01-05 01:46:10 +0600

    End of section 16

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -11,7 +11,23 @@ namespace API.Extenstions
             // Add Swagger 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkiNet API", Version = "v1" });
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT auth Bearer Shcheme",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+            c.AddSecurityDefinition("Bearer", securitySchema);
+            var securityRequirement = new OpenApiSecurityRequirement{ { securitySchema, new[] { "Bearer" } } };
+                c.AddSecurityRequirement(securityRequirement);
             });
 
             return services;
