e721d938fd413045406f0f42f93d148b1beac66b
unknown <mobasshirice@gmail.com>
2023-01-05 16:05:13 +0600

    End of section 18

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -14,9 +14,11 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   constructor(private busyService: BusyService) { }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.busyService.busy();
-    return next.handle(request).pipe(
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (!req.url.includes("emailexists")) {
+      this.busyService.busy();
+    }
+    return next.handle(req).pipe(
       delay(1000),
       finalize(() => {
         this.busyService.idle();
